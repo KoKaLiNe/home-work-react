917ea35a6469f0652aebcfed909b9628e0ee50ff
KoKa_LiNe <whitekokaline@gmail.com>
2022-04-15 17:59:32 +0300

    add: Прокт сделан по лекции до момента, когда можно делать ДЗ

---
+++ src/components/app/app.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Main from "../../pages/main/main";
+// import Archive from "../../pages/archive/archive";
+import { AppRoute } from "../../const";
+
+const App = () => {
+    return (
+        <Main />
+        // <Archive mode={AppRoute.ARCHIVE} />
+    )
+};
+
+export default App;
---
+++ src/pages/archive/archive.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Board from "../../components/board/board";
+import Header from "../../components/header/header";
+import { AppRoute } from "../../const";
+
+const Archive = () => {
+    return (
+        <React.Fragment>
+            <Header mode={AppRoute.ARCHIVE}/>
+            <section className="main__wrapper">
+                <Board />
+            </section>
+        </React.Fragment>
+    )
+};
+
+export default Archive;
