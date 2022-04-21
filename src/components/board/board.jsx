1c2bc8bd3558aed5a19130a93008fbe51e0f855d
Kholina Dasha <holina-dasha@mail.ru>
2022-04-22 02:58:18 +0300

    исправлено дублирование кода в board

--- src/components/board/board.jsx
+++ src/components/board/board.jsx
@@ -2,24 +2,9 @@ import React from "react";
 import Card from "../card/card";
 import LoadMore from "../load-more/load-more";
 import Sorting from "../sorting/sorting";
-import { AppRoute } from "../../const";
-import { useLocation } from "react-router-dom";
 
 const Board = () => {
-    const { pathname } = useLocation();
-    if (pathname === AppRoute.ARCHIVE) {
         return (
-            // Страница с сортировкой
-            <section className="board">
-                <div className="board__events">
-                    <Card />
-                </div>
-                <LoadMore />
-            </section>
-        )
-    } else {
-        return (
-            // Страница без сортировки
             <section className="board">
                 <Sorting />
                 <div className="board__events">
@@ -28,7 +13,6 @@ const Board = () => {
                 <LoadMore />
             </section>
         )
-    }
 };
 
 export default Board;
--- src/components/sorting/sorting.jsx
+++ src/components/sorting/sorting.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { AppRoute } from "../../const";
+import { useLocation } from "react-router-dom";
 
 const Sorting = () => {
-    return (
-        <div className="board__filter-list">
+    const { pathname } = useLocation();
+    if (pathname === AppRoute.MAIN) {
+        return (
+            <div className="board__filter-list">
             <span className="board__filter--title">Сортировка:</span>
             <input
                 type="radio"
@@ -10,24 +14,25 @@ const Sorting = () => {
                 className="board__filter visually-hidden"
                 name="board-filter"
                 checked
-            />
+                />
             <label for="board__filter-default" className="board__filter-label">По умолчанию</label>
             <input
                 type="radio"
                 id="board__filter-new"
                 className="board__filter visually-hidden"
                 name="board-filter"
-            />
+                />
             <label for="board__filter-new" className="board__filter-label">Сначала новые</label>
             <input
                 type="radio"
                 id="board__filter-old"
                 className="board__filter visually-hidden"
                 name="board-filter"
-            />
+                />
             <label for="board__filter-old" className="board__filter-label">Сначала старые</label>
         </div>
     )
+}
 };
 
 export default Sorting;
